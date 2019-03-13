6603652b849fe95e027d2881e948daf128756e2c
fedostseva <fedostseva@reksoft.ru>
2019-03-13 13:46:48 +0300

    modified Home Task 2 - deleted main method

--- Lessons/src/main/java/ru/java/courses/Lesson2.java
+++ Lessons/src/main/java/ru/java/courses/Lesson2.java
@@ -1,9 +1,5 @@
 package ru.java.courses;
 
-import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 class Lesson2 {
 
     /**
@@ -35,25 +31,9 @@ class Lesson2 {
      */
     static String initials(String fullName) {
         String[] shortName = fullName.split(" ");
-        String initName = shortName[0].charAt(0) + ". " + shortName[1].charAt(0) + ". " + shortName[2].charAt(0) + ".";
-        return initName;
+        String initName = shortName[0].charAt(0) + "." + shortName[1].charAt(0) + "." + shortName[2].charAt(0) + ".";
+        return initName.toUpperCase();
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Enter x");
-        int y;
-        Scanner sc = new Scanner(System.in);
-        y = sc.nextInt();
-        System.out.println (formula (y));
-
-        System.out.println("Enter full name");
-        String fullName;
-        Scanner name = new Scanner (System.in);
-        fullName = name.nextLine();
-        System.out.print(initials (initials(fullName.toUpperCase())));
-
-
-
-    }
 }
 
